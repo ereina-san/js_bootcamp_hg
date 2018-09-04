e6d4e65bbc333118d395c6b9c3d3de77486f0958
hotelgirl <redstain@ukr.net>
2018-09-04 14:37:52 +0300

    first c

---
+++ myapp.js
@@ -0,0 +1,14 @@
+var morning = {
+    weather: getWeather(){console.log="Its sunny today!"};
+    breakfast: makeCoffee(sugar)
+    {
+        var cup = "Boil water,";
+        cup += " pour into cup,";
+        cup += " add coffee,";
+        if (sugar==0) 
+            cup+= " add no sugar";
+        else
+        cup += " add " + sugar + " spoons of sugar";
+        return cup;
+    }
+}
